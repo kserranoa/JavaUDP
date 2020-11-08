978ee451e17a119272a109832a385cf6705e7c33
Karina <kserranoa16@gmail.com>
2020-11-08 17:00:44 -0600

    creacion metodos en clase main ahorcado

--- src/main/java/JAVAUDP/Menu.java
+++ src/main/java/JAVAUDP/Menu.java
@@ -5,49 +5,75 @@ import java.util.Scanner;
 public class Menu {
 
     public static void main(String[] args) {
-        //Palabras que el sistema puede escoger aleatoriamente
-        String [] palabras = {"perro", "mono", "gato"};
+        String palabra = eligePalabra();
+        char [ ] guiones = cambioGuiones(palabra);
         
-        //Selecionar palabra de manera aleatoria
-        Random a = new Random();
-        int n = a.nextInt(palabras.length);
-        String opcionPalabra = palabras[n];
-        //System.out.println(opcionPalabra);
-        
-        //Contar la cantidad de letras que tiene la palabra
-        int cantidadLetras = opcionPalabra.length();
-        
-        //Recorrer las letras que contiene la palabra selecionada
-        char [] letrasPalabra = new char[cantidadLetras];        
-        
-        //Convertir la cantidad de letras en guiones bajos
-        for (int i = 0; i < letrasPalabra.length; i++) {            
-            letrasPalabra[i] = '_';            
-        }
-        
-        //Crear variable para deterrminar si el juego esta terminado o no
-        boolean gameover = false;
-        
-        //Solicitar una letra al usuario
-        Scanner entrada = new Scanner(System.in);
-        
-        //Hacer un ciclo para ver si la letra ingresada por el usuairo esta dentro de la palabra escogida
-        do {
+        // Permite que el usuario ingrese las letras para adivinar la palabra
+        boolean terminaJuego = false; // permite que el juego termine
+        Scanner lectura = new Scanner(System.in);  //Solicita una letra al usuario 
+        int intentos = 3;
+       
+        do{
+            System.out.println("Te quedan " + intentos + " intentos");
+            System.out.println(guiones);
             System.out.println("Introduce una letra: ");
-            //Usar solo la primera letra ingresada por el usuario
-            char letra = entrada.next().charAt(0);
-            
-            //Crear una variable para ver si acierto letra
-            boolean acierto = false;
-            
-            for (int i = 0; i < cantidadLetras; i++) {
-                if (opcionPalabra.charAt(i) == letra) {
-                    letrasPalabra[i] = letra;
-                    acierto = true;
+            char letra = lectura.next().charAt(0); //toma la letra ingresada por el usuario
+            boolean letraAcertada = false;
+            // Revisa si la letra esta en la palabra
+            for(int i = 0; i < palabra.length(); i++){
+                if(palabra.charAt(i) == letra){ // si la letra esta en la palabra
+                    guiones[i] = letra; //cambia el guion por la letra
+                    letraAcertada = true;
                 }
             }
-        } while (gameover);
+            if(!letraAcertada){ // Si la letra no esta en la palabra 
+                System.out.println("Esa letra no esta en la palabra. Ingresa otra.");
+                -- intentos;
+                if(intentos == 0 ) {
+                    System.out.println("Has perdido. Se agotaron los intentos.");
+                    terminaJuego = true; // Permite que el juego pare de ejecutarse
+                }
+            } else{
+               boolean GanoJuego = !verificaGuiones(guiones); //verifica si ha ganado, si no hay ningun guion
+               if(GanoJuego){
+                   System.out.println("Has ganado el juego");
+                   terminaJuego = true; // Permite que el juego pare de ejecutarse
+               }
+            }
+        } while(!terminaJuego);
         
+        lectura.close(); // Termina de pedir las letras porque ha ganado
+      
+    }
+    
+    // Metodo para generar la palabra 
+    static String eligePalabra(){
+        String [ ] listPalabras = {"vaca", "perro", "cabra", "mono"};
+        Random random = new Random();
+        int n = random.nextInt(listPalabras.length);
+        return listPalabras[n];
+    }
+    
+    //Metodo para cambiar palabra a guiones
+    static char[ ] cambioGuiones(String palabra){
+        
+        int cantLetras = palabra.length();
+        char [ ] guiones = new char[cantLetras];
+        
+        for(int i = 0; i < guiones.length; i++){
+            guiones[i] = '_';
+        }
+        
+        return guiones;
+        
+    }
+    
+    // Metodo que verifica si hay guiones en la palabra
+    static boolean verificaGuiones(char[] array){
+        for(char l: array){
+            if(l == '_') return true;
+        }
+        return false;
     }
 
 }
